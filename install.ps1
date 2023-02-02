3a0ae0b2d7a968ec5b9a93ceff17a29c6b234e9a
Gage Lodba <38990407+JerimiahOfficial@users.noreply.github.com>
2023-02-02 03:44:21 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,13 +1,12 @@
 # Web Server Administration - Test 1 script
 # This script is meant to be run on a Windows Server 2012 machine
 
+# Create a variable for the student number
+$StudentNumber = "000855799"
+
 # Pre-running steps:
 #   Install google chrome
 #   Install Nartac's IIS Crypto
-
-# Set the window title and error action preference
-$Host.UI.RawUI.WindowTitle = "Test 1 script - Web Server Administration"
-$ErrorActionPreference = "Stop"
 
 # Check if the following tasks are scheduled and if they are end them
 if (Get-ScheduledTask | Where-Object { $_.TaskName -eq "ResumeWorkflows" }) {
@@ -23,41 +22,34 @@ if (!(Test-Path "C:\resume.ps1")) {
     New-Item -Path "C:\resume.ps1" -ItemType File -Value "Import-Module PSWorkflow`nGet-Job -State Suspended | Resume-Job -Wait | Wait-Job"
 }
 
-# Final setup workflow
+# Workflow to complete the lab
 Import-Module PSWorkflow
 workflow CompleteLab {
-    # Create a variable for the student number
-    $StudentNumber = "000855799"
-
-    # Elevate the PowerShell execution policy to (Bypass).
-    Set-ExecutionPolicy -ExecutionPolicy Bypass
-
-    # Rename the computer to WS000855799
-    Rename-Computer -NewName "WS$StudentNumber"
-
-    # Set the computers time zone to (Eastern Standard Time US & Canada)
-    InlineScript {
-        Invoke-Command -ScriptBlock {
-            Set-TimeZone -Id "Eastern Standard Time"
-        }
+    function InitialSetup {
+        # Elevate the PowerShell execution policy to (Bypass).
+        Set-ExecutionPolicy -ExecutionPolicy Bypass
+    
+        # Rename the computer to WS000855799
+        Rename-Computer -NewName "WS$StudentNumber"
+    
+        # Set the computers time zone to (Eastern Standard Time US & Canada)
+        Set-TimeZone -Id "Eastern Standard Time"
+    
+        # Disable all firewall profiles
+        Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False
+    
+        # Get the first InterfaceIndex of the network adapter
+        $InterfaceIndex = Get-NetAdapter | Select-Object -First 1 | Select-Object -ExpandProperty InterfaceIndex
+    
+        # Set a static IP address of 192.168.100.20 and a static subnet mask of 255.255.255.0
+        New-NetIPAddress -InterfaceIndex $InterfaceIndex -IPAddress 192.168.100.20 -PrefixLength 24 -AddressFamily IPv4
+        Set-NetIPInterface -InterfaceIndex $InterfaceIndex -AddressFamily IPv4 -Dhcp Disabled
+    
+        # Restart the computer
+        Restart-Computer
     }
     
-    # Disable all firewall profiles
-    Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False
-
-    # Get the first InterfaceIndex of the network adapter
-    $InterfaceIndex = Get-NetAdapter | Select-Object -First 1 | Select-Object -ExpandProperty InterfaceIndex
-
-    # Set the network adapter to use a static IP address
-    Set-NetIPInterface -InterfaceIndex $InterfaceIndex -AddressFamily IPv4 -Dhcp Disabled
-
-    # Set a static IP address of (192.168.100.20) and a static subnet mask of (255.255.255.0)
-    Set-NetIPAddress -InterfaceIndex $InterfaceIndex -IPAddress 192.168.100.20 -PrefixLength 24
-
-    # Restart the computer
-    Restart-Computer
-
-    InlineScript {
+    function InstallRequirements {
         # An array containing all the required features
         $Features = @(
             "AD-Domain-Services", 
@@ -78,6 +70,7 @@ workflow CompleteLab {
             "Web-Basic-Auth", 
             "Web-Mgmt-Tools", 
             "Web-Mgmt-Console",
+            "Web-Mgmt-Service",
             "GPMC", 
             "RSAT", 
             "RSAT-Role-Tools", 
@@ -88,39 +81,86 @@ workflow CompleteLab {
             "RSAT-ADDS-Tools", 
             "RSAT-DNS-Server"
         )
-
+    
         # Install the required features
-        $Features.ForEach({
-                Install-WindowsFeature -Name $_ -IncludeManagementTools
-            })
-
-        # Promote the server to a domain controller
-        Install-ADDSForest `
-            -DomainName "d$StudentNumber.com" `
-            -DomainNetbiosName "D$StudentNumber" `
-            -ForestMode "WinThreshold" `
-            -DomainMode "WinThreshold" `
-            -InstallDns:$true `
-            -DatabasePath "C:\Windows\NTDS" `
-            -LogPath "C:\Windows\NTDS" `
-            -SysvolPath "C:\Windows\SYSVOL" `
-            -Force:$true
-
-        # Create the websites
-        $Websites = @( "sales.$StudentNumber.au", "www.$StudentNumber.co.uk", "donations.wildlife-$StudentNumber.org")
-        $Websites.ForEach({
-                New-Item -Path "C:\Data\$_" -ItemType Directory
-                New-Item -Path "C:\Data\$_\homepage.html" -ItemType File
-            })
-
-        # TODO: Add the rest of the steps.
+        Install-WindowsFeature -Name $Features -IncludeManagementTools
     }
+    
+    function PromoteToController {
+        # Promote the server to a domain controller
+        if (!(Get-WmiObject -Query "SELECT * FROM Win32_ComputerSystem" | where-object { $_.Domain -eq "d$StudentNumber.com" })) {
+            Import-Module ADDSDeployment
+            Install-ADDSDomainController `
+                -InstallDns `
+                -Credential (Get-Credential)`
+                -DomainName ("d$StudentNumber.com") `
+        
+        }
+    }
+    
+    function CreateWebsites {
+        # Delete the default website if it exists
+        Import-Module WebAdministration
+        if (Get-WebSite | Where-Object { $_.Name -eq "Default Web Site" }) {
+            Remove-WebSite -Name "Default Web Site"
+        }
+    
+        # Create a array containing all the websites
+        $Websites = @("sales.$StudentNumber.au", "www.$StudentNumber.co.uk", "donations.wildlife-$StudentNumber.org")
+        $Ports = @("80", "443", "8080")
+    
+        # Create the websites in IIS
+        Import-Module WebAdministration
+        $j = 0
+        $Websites.ForEach({
+                New-WebSite -Name $_ -IPAddress "192.168.100.20" -Port $Ports[$j] -PhysicalPath "C:\Data\$_" -Force
+            })
+    
+        # Create Dns records for the websites
+        Import-Module DnsServer
+        $i = 0
+        $Websites.ForEach({
+                Add-DnsServerResourceRecordA -ZoneName "d$StudentNumber.com" -Name $_ -IPv4Address "192.168.100.2$i"
+                $i++
+            })
+        
+        # Enable HSTS for the websites to make them secure
+        $sitesCollection = Get-IISConfigSection -SectionPath "system.applicationHost/sites" | Get-IISConfigCollection
+        $sitesCollection.foreach({
+                Set-IISConfigAttributeValue -SectionPath "system.applicationHost/sites" -CollectionPath "sites/site[@name='$($_.Name)']" -AttributePath "hsts[@enabled='true']/@maxAge" -Value "31536000"
+                Set-IISConfigAttributeValue -SectionPath "system.applicationHost/sites" -CollectionPath "sites/site[@name='$($_.Name)']" -AttributePath "hsts[@enabled='true']/@includeSubDomains" -Value "true"
+                Set-IISConfigAttributeValue -SectionPath "system.applicationHost/sites" -CollectionPath "sites/site[@name='$($_.Name)']" -AttributePath "hsts[@enabled='true']/@redirectHttpToHttps" -Value "true"
+                Set-IISConfigAttributeValue -SectionPath "system.applicationHost/sites" -CollectionPath "sites/site[@name='$($_.Name)']" -AttributePath "hsts[@enabled='true']/@preload" -Value "true"
+                Set-IISConfigAttributeValue -SectionPath "system.applicationHost/sites" -CollectionPath "sites/site[@name='$($_.Name)']" -AttributePath "hsts[@enabled='true']/@confirm" -Value "true"
+            })
+    
+        # Create the SAN/UC certificate:
+        New-SelfSignedCertificate -DnsName "sales.$StudentNumber.au", "www.$StudentNumber.co.uk", "donations.wildlife-$StudentNumber.org" -CertStoreLocation cert:\LocalMachine\My -FriendlyName "SAN/UC Certificate"
+    
+        # Import the module for managing IIS
+        [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.Web.Management") 
+        [Microsoft.Web.Management.Server.ManagementAuthentication]::CreateUser("User1", "P@ssw0rd")
+        [Microsoft.Web.Management.Server.ManagementAuthorization]::Grant("User1", "sales.$StudentNumber.au", $true)
+    }
+
+    "Performing initial setup..."
+    InitialSetup
+
+    "Installing requirements..."
+    InstallRequirements
+
+    "Promoting to a domain controller..."
+    PromoteToController
+
+    "Creating websites..."
+    CreateWebsites
+    Exit
 }
 
 $TaskName = "ResumeWorkflows"
 $PS = $PSHOME + "\powershell.exe"
 $Action = New-ScheduledTaskAction -Execute $PS -Argument " -executionpolicy bypass c:\resume.ps1"
 $Trigger = New-ScheduledTaskTrigger -AtLogOn
-Register-ScheduledTask -TaskName $TaskName -Action $Action -Trigger $Trigger -RunLevel Highest
 
+Register-ScheduledTask -TaskName $TaskName -Action $Action -Trigger $Trigger -RunLevel Highest
 CompleteLab -AsJob -JobName $(get-date -Format hhmmss)
